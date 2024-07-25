e8e69b247aeb594d1697c74bad7e4be06ace9804
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2024-07-25 10:37:34 +0200

    [Fix] Correction des  contraintes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import ClockPage from './clockpage';
-import TimerPage from './timerpage';
+import ClockPage from './Clock/clockpage';
+import TimerPage from './Timer/timerpage';
+import CalendarPage from './Calendar/calendarpage';
+import UserFormPage from './Form/formpage';
 import './App.css'; // Import global styles
 
 const App = () => {
@@ -13,13 +15,19 @@ const App = () => {
           <ul>
             <li><Link to="/">Clock</Link></li>
             <li><Link to="/timer">Timer</Link></li>
+            <li><Link to="/calendar">Calendar</Link></li>
+            <li><Link to="/form">Form</Link></li>
           </ul>
         </nav>
-        {/* Routes */}
-        <Routes>
-          <Route path="/" element={<ClockPage />} />
-          <Route path="/timer" element={<TimerPage />} />
-        </Routes>
+        {/* Main Content */}
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<ClockPage />} />
+            <Route path="/timer" element={<TimerPage />} />
+            <Route path="/calendar" element={<CalendarPage />} />
+            <Route path="/form" element={<UserFormPage />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
